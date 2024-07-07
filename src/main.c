0593cc2daf1ca9dda69935ffd4638ab50085ae8e
AnirudhMathur12 <126768505+AnirudhMathur12@users.noreply.github.com>
2024-07-07 22:13:33 +0530

    Add a file read/dump thingy. I should probably create soemthing like a utils folder for it.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/_types/_size_t.h>
+
+char *read_file(char *filename);
 
 int main(int argc, char **argv) {
-    printf("I write bad code. ");
+    char *file_content = read_file(argv[1]);
     return 0;
+}
+
+char *read_file(char *filename) {
+    FILE *source;
+
+    source = fopen(filename, "r");
+    if (source == NULL) {
+        printf("Error: File not found or could not open it.\n");
+        return NULL;
+    }
+
+    fseek(source, 0, SEEK_END);
+    size_t file_size_bytes = ftell(source);
+    fseek(source, 0, SEEK_SET);
+
+    char *buffer = (char *)malloc(file_size_bytes + 1);
+
+    fread(buffer, 1, file_size_bytes, source);
+    buffer[file_size_bytes] = '\0';
+
+    fclose(source);
+    free(source);
+
+    return buffer;
 }
